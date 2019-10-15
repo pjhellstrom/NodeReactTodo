c004b90380df2fa62fe7f111b938e645193435d3
pjhellstrom <pjh.hellstrom@gmail.com>
2019-10-15 10:12:17 -0400

    Configured for Heroku deployment

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
